52d6dba32fdc237aa019655b1ed64417a39a6fd2
Will Holcomb <will@dhappy.org>
2020-05-02 02:52:51 -0400

    git rm . -r 👛

--- Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	go build -o cmd/git-remote-ipfs/git-remote-ipfs ./cmd/git-remote-ipfs/...
-
-test:
-	go test -v ./...
-
-.PHONY: all test
--- doc.go
+++
@@ -1,6 +0,0 @@
-// git-remote-ipfs provides 3 git remotes:
-// * git-remote-ipvfs: allows to import git repositories to IPFS with a .git/vfs directory providing access to all the commits.
-// * git-remote-ipfs: same as ipvfs without the vfs filesystem
-// * git-remote-ipns: push to and fetch from IPNS names
-
-package remote
